3406622489a088415901dc454a581e6ffca9f135
ashle <amireles1334@yahoo.com>
2023-02-15 02:00:30 -0600

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-  import NavMenu from '@/components/NavMenu.vue'
-  import MainPage from '@/components/MainPage.vue'
+  import NavMenu from '@/components/NavBar.vue'
 </script>
 
 <template>
   <NavMenu />
-  <MainPage />
+  <RouterView />
 </template>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2 class="not-found">404:<br />Page Not Found</h2>
+</template>
+
+<style lang="postcss" scoped>
+    .not-found{
+        @apply text-center text-8xl font-bold text-red-900 pt-80
+}
+</style>
+
+
