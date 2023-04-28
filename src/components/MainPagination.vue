e57c6bb483e6b18e175c234174710a2a6a2d6d65
ashle <amireles1334@yahoo.com>
2023-04-28 16:30:11 -0500

    add pagination api

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,41 +1,56 @@
 <script setup>
-import { ref } from 'vue'
-const pages = ref(10)
-const activePage = ref(4)
+import useApi from '@/composables/useApi'
 
-const prevPage = () => {
-    if (activePage.value > 1) {
-        activePage.value--
-    }
+const { activePage, pages, getEmployees } = useApi()
+
+const prevPage = async () => {
+  if (activePage.value > 1) {
+    activePage.value--
+    await getEmployees()
+  }
 }
-const nextPage = () => {
-    if (activePage.value < pages.value) {
-        activePage.value++
-    }
+const nextPage = async () => {
+  if (activePage.value < pages.value) {
+    activePage.value++
+    await getEmployees()
+  }
+}
+const jumpPage = async (page) => {
+    activePage.value = page
+    await getEmployees()
 }
 </script>
 
 <template>
-    <div class="pagination">
-        <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
-        <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page"> 
-            {{ page }}
-        </button>
-        <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
-    </div>
+  <div class="pagination">
+    <button class="action" :disabled="activePage === 1" @click="prevPage">
+      Prev
+    </button>
+    <button
+      v-for="page in pages"
+      class="page"
+      :class="page === activePage ? 'active' : ''"
+      @click="jumpPage(page)"
+    >
+      {{ page }}
+    </button>
+    <button class=" :disabled="activePage === pages" @click="nextPage">
+      Next
+    </button>
+  </div>
 </template>
 
 <style scoped lang="postcss">
-    .pagination{
-        @apply flex justify-center gap-5;
-        .action{
-        @apply rounded-md bg-blue-50 p-2 font-medium text-slate-800 shadow-md hover:bg-blue-300 disabled:text-slate-400 hover:disabled:bg-red-800;
-        }
-        .page{
-            @apply rounded-md bg-blue-50 p-2 font-medium text-slate-800 shadow-md hover:bg-blue-200;
-            &.active{
-                @apply bg-sky-900 text-slate-100 hover:bg-blue-400 hover:text-slate-800 hover:font-bold;
-            }
-        }
+.pagination {
+  @apply flex justify-center gap-5;
+  .action {
+    @apply rounded-md bg-blue-50 p-2 font-medium text-slate-800 shadow-md hover:bg-blue-300 disabled:text-slate-400 hover:disabled:bg-red-800;
+  }
+  .page {
+    @apply rounded-md bg-blue-50 p-2 font-medium text-slate-800 shadow-md hover:bg-blue-200;
+    &.active {
+      @apply bg-sky-900 text-slate-100 hover:bg-blue-400 hover:font-bold hover:text-slate-800;
     }
+  }
+}
 </style>
